94fa62d7041f8f803e0a31ddd6e3f15d08b89158
Alex Ghoust <ghoust@pinacotheca.net>
2023-12-08 11:40:17 +0600

    Add elyses-destructured-enchantments

---
+++ elyses-destructured-enchantments/enchantments.js
@@ -0,0 +1,67 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Get the first card in the given deck
+ *
+ * @param {Card[]} deck
+ *
+ * @returns {Card} the first card in the deck
+ */
+export function getFirstCard(deck) {
+  var [x, ...rest] = deck;
+  return x;
+}
+
+/**
+ * Get the second card in the given deck
+ *
+ * @param {Card[]} deck
+ *
+ * @returns {Card} the second card in the deck
+ */
+export function getSecondCard(deck) {
+  var [x1, x2, ...rest] = deck;
+  return x2;
+}
+
+/**
+ * Switch the position of the first two cards in the given deck
+ *
+ * @param {Card[]} deck
+ *
+ * @returns {Card[]} new deck with reordered cards
+ */
+export function swapTopTwoCards(deck) {
+  var [x1, x2, ...rest] = deck;
+  return [x2, x1, ...rest];
+}
+
+/**
+ * Put the top card of the given deck into a separate discard pile
+ *
+ * @param {Card[]} deck
+ *
+ * @returns {[Card, Card[]]} the top card of the given
+ * deck and a new deck containing all the other cards
+ */
+export function discardTopCard(deck) {
+  var [x, ...rest] = deck;
+  return [x, rest];
+}
+
+/** @type {Card[]} **/
+const FACE_CARDS = ['jack', 'queen', 'king'];
+
+/**
+ * Insert face cards into the given deck
+ *
+ * @param {Card[]} deck
+ *
+ * @returns {Card[]} new deck where the second,
+ * third, and fourth cards are the face cards
+ */
+export function insertFaceCards(deck) {
+  var [x, ...rest] = deck;
+  return [x, ...FACE_CARDS, ...rest];
+}
