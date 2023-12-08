add9d13baefc77cd6c666668071ba0ccedc0f32b
Alex Ghoust <ghoust@pinacotheca.net>
2023-12-08 11:42:56 +0600

    Add resistor-color

---
+++ resistor-color/resistor-color.js
@@ -0,0 +1,16 @@
+export const colorCode = (val) => {
+  return COLORS.indexOf(val);
+};
+
+export const COLORS = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white"
+];
