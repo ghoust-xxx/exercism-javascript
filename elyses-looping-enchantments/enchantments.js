bd7f26fa4f7e5004e5268a51581ab17afe81e36a
Alex Ghoust <ghoust@pinacotheca.net>
2023-12-08 11:40:29 +0600

    Add elyses-looping-enchantments

---
+++ elyses-looping-enchantments/enchantments.js
@@ -0,0 +1,30 @@
+// @ts-check
+
+/**
+ * Determine how many cards of a certain type there are in the deck
+ *
+ * @param {number[]} stack
+ * @param {number} card
+ *
+ * @returns {number} number of cards of a single type there are in the deck
+ */
+export function cardTypeCheck(stack, card) {
+  let out = 0;
+  stack.forEach((n, i) => { if (n === card) out++; });
+  return out;
+}
+
+/**
+ * Determine how many cards are odd or even
+ *
+ * @param {number[]} stack
+ * @param {boolean} type the type of value to check for - odd or even
+ * @returns {number} number of cards that are either odd or even (depending on `type`)
+ */
+export function determineOddEvenCards(stack, type) {
+  let out = 0;
+  for (let i of stack) {
+    if ((i % 2 === 0) === type ) out++;
+  }
+  return out;
+}
